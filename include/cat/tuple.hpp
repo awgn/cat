5ea98f9db75ec32133e81f6505670239688d60d9
Nicola Bonelli <nicola.bonelli@gmail.com>
2015-02-12 14:28:02 +0100

    Added utility functions for std::tuple and related tests.

---
+++ include/cat/tuple.hpp
@@ -0,0 +1,116 @@
+/******************************************************************************
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-15 Nicola Bonelli <nicola@pfq.io>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ ******************************************************************************/
+
+#pragma once
+
+#include <tuple>
+#include <utility>
+
+namespace cat
+{
+    //
+    // implementation details...
+    //
+
+    namespace details
+    {
+        template <size_t O, typename ...Xs, typename ... Ys, size_t ...N>
+        void tuple_assign(std::tuple<Xs...> &lhs, std::tuple<Ys...> const &rhs, std::index_sequence<N...>)
+        {
+            std::initializer_list<bool> sink {(std::get<N>(lhs) = std::get<N>(rhs), true)...};
+            (void)sink;
+        }
+
+        template <typename Fun, typename TupleT, size_t ...N>
+        void tuple_foreach(Fun fun, TupleT &&tup, std::index_sequence<N...>)
+        {
+            std::initializer_list<bool> sink {(fun(std::get<N>(std::forward<TupleT>(tup))), true)...};
+            (void)sink;
+        }
+
+        template <typename Fun, typename TupleT, size_t ...N>
+        auto tuple_map(Fun fun, TupleT &&tup, std::index_sequence<N...>)
+        {
+            return std::make_tuple(fun(std::get<N>(std::forward<TupleT>(tup)))...);
+        }
+
+        template <typename Fun, typename TupleT, size_t ...N>
+        auto tuple_apply(Fun fun, TupleT &&tup, std::index_sequence<N...>)
+        {
+            return fun(std::get<N>(std::forward<TupleT>(tup))...);
+        }
+    }
+
+    //
+    // tuple_assign: assign a tuple (or a sub-tuple) to a tuple
+    //
+
+    template <typename ...Xs, typename ... Ys, size_t Offset = 0>
+    void tuple_assign(std::tuple<Xs...> &lhs, std::tuple<Ys...> const &rhs)
+    {
+        return details::tuple_assign<Offset>(lhs, rhs, std::make_index_sequence<sizeof...(Ys)>());
+    }
+
+    //
+    // tuple_foreach: polymorphic side effects over tuple (like mapM_)
+    //
+
+    template <typename Fun, typename TupleT>
+    void tuple_foreach(Fun fun, TupleT &&tup)
+    {
+        return details::tuple_foreach(fun,
+                                      std::forward<TupleT>(tup),
+                                      std::make_index_sequence<
+                                        std::tuple_size<typename std::decay<TupleT>::type>::value>());
+    }
+
+    //
+    // tuple_map: fmap over tuple
+    //
+
+    template <typename Fun, typename TupleT>
+    auto tuple_map(Fun fun, TupleT &&tup)
+    {
+        return details::tuple_map(fun,
+                                  std::forward<TupleT>(tup),
+                                  std::make_index_sequence<
+                                        std::tuple_size<typename std::decay<TupleT>::type>::value>());
+    }
+
+    //
+    //  tuple_apply: expand a tuple and pass as pack to a callable
+    //
+
+    template <typename Fun, typename TupleT>
+    auto tuple_apply(Fun fun, TupleT && tup)
+    {
+        return details::tuple_apply(fun,
+                                   std::forward<TupleT>(tup),
+                                   std::make_index_sequence<
+                                        std::tuple_size<typename std::decay<TupleT>::type>::value>());
+    }
+
+} // namespace cat
---
+++ test/test/test-tuple.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <cat/tuple.hpp>
+
+#include <yats.hpp>
+
+using namespace yats;
+using namespace cat;
+
+Context(tuple)
+{
+    Test(apply)
+    {
+        auto add = [](int a, int b) { return a+b; };
+
+        std::tuple<int, int> tup{40,2};
+
+        Assert(tuple_apply(add, tup), is_equal_to(42));
+    }
+
+    Test(apply_const)
+    {
+        auto add = [](int a, int b) { return a+b; };
+
+        std::tuple<int, int> const tup{40,2};
+
+        Assert(tuple_apply(add, tup), is_equal_to(42));
+
+    }
+
+    Test(apply_forward)
+    {
+        auto mistery = [](int &a, int b) -> int {
+                a = 10;
+            return a+b; };
+
+        int a = 40;
+
+        Assert(tuple_apply(mistery, std::forward_as_tuple(a,2)), is_equal_to(12));
+        Assert(a, is_equal_to(10));
+    }
+
+
+    template <typename T>
+    void test(T &v)
+    {
+        long int sum = 0;
+        tuple_foreach([&](auto n) { sum += n; }, v);
+    }
+
+    Test(for_each)
+    {
+        std::tuple<int,int,int> v {1,2,3};
+        std::tuple<long, long> q {1,2};
+
+        test(v);
+        test(q);
+    }
+
+    Test(for_each_const)
+    {
+        std::tuple<int,int,int> const v {1,2,3};
+        int sum = 0;
+
+        tuple_foreach([&](int n) {
+                              sum += n;
+                             }, v);
+
+        Assert(sum, is_equal_to(6));
+    }
+
+    Test(for_each_forward)
+    {
+        int a = 1, b = 2, c = 3;
+        int sum = 0;
+
+        tuple_foreach([&](int n) {
+                              sum += n;
+                             }, std::forward_as_tuple(a,b,c));
+
+        Assert(sum, is_equal_to(6));
+    }
+
+    Test(map)
+    {
+        auto t = tuple_map(strlen, std::make_tuple("hello", "world"));
+
+        size_t len = 0;
+
+        tuple_foreach([&len](size_t n) { len+=n; }, t);
+
+        Assert(len, is_equal_to(size_t(10)));
+    }
+
+    Test(map_const)
+    {
+        auto const t = std::make_tuple("hello", "world");
+
+        auto ts = tuple_map(strlen, t);
+
+        size_t len = 0;
+
+        tuple_foreach([&len](size_t n) { len+=n; }, ts);
+
+        Assert(len, is_equal_to(size_t(10)));
+    }
+
+
+
+    namespace noncopyable
+    {
+        struct type
+        {
+            type() = default;
+            type(type &&) = default;
+            type(const type &) = delete;
+        };
+    }
+
+    Test(non_copyable)
+    {
+        auto t = std::make_tuple(noncopyable::type(), noncopyable::type());
+        tuple_foreach([](auto) { }, std::move(t));
+    }
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    return yats::run(argc, argv);
+}
