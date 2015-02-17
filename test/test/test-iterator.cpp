38c95c28ae564b8b1e73f345676837a1567581bc
Nicola Bonelli <nicola.bonelli@gmail.com>
2015-02-17 18:41:02 +0100

    test-iterator added.

---
+++ test/test/test-iterator.cpp
@@ -0,0 +1,33 @@
+#include <cat/iterator.hpp>
+
+#include <yats.hpp>
+
+using namespace yats;
+using namespace cat;
+
+
+// Tests:
+//
+
+Context(iterator)
+{
+    Test(map_iterator)
+    {
+        std::vector<std::string> v { "one", "two", "three" };
+
+        auto s = [](const std::string &e) { return e.size(); };
+
+        std::vector<size_t> r { map_iterator( std::begin(v), s),
+                                map_iterator( std::end(v), s) };
+
+        Assert( r == std::vector<size_t> {3, 3, 5} );
+    }
+}
+
+
+int
+main(int argc, char*  argv[])
+{
+    return yats::run(argc, argv);
+}
+
